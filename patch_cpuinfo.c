528b9772069ee9924026069614fd002dfab4bc5d
kmou424 <me@kmou424.moe>
2024-10-19 00:23:11 +0800

    feat: expose sysfs node to patch cpu model name

--- patch_cpuinfo.c
+++ patch_cpuinfo.c
@@ -1,22 +1,57 @@
 #include <linux/module.h>
-#include <linux/kernel.h>
-#include <linux/init.h>
-#include <linux/sched.h>
-#include <linux/proc_fs.h>
-#include <linux/uaccess.h>
 
-static int __init patch_cpuinfo_init(void) {
-    struct cpuinfo_x86 *cpuinfo;
-    
-    cpuinfo = &cpu_data(0);
+static struct kobject *patch_cpuinfo_kobj;
 
-    strncpy(cpuinfo->x86_model_id, "Intel(R) Core(TM) i9-13900T ES", sizeof(cpuinfo->x86_model_id));
+static void patch_model_name(const char *model_name) {
+    struct cpuinfo_x86 *cpuinfo = &cpu_data(0);
+
+    strncpy(cpuinfo->x86_model_id, model_name, sizeof(cpuinfo->x86_model_id));
+    cpuinfo->x86_model_id[sizeof(cpuinfo->x86_model_id) - 1] = '\0';
 
     printk(KERN_INFO "patch_cpuinfo: model name has been changed to %s\n", cpuinfo->x86_model_id);
+}
+
+static ssize_t write_model_name(struct kobject *kobj, struct kobj_attribute *attr, const char *buf, size_t count) {
+    char model_name[64];
+    size_t len = min(count, sizeof(model_name));
+
+    strncpy(model_name, buf, len);
+
+    // put end sign to last pos
+    model_name[len] = '\0';
+
+    // breakline in string, put end sign
+    int breakline_pos = strcspn(model_name, "\n");
+    if (breakline_pos < sizeof(model_name))
+        model_name[breakline_pos] = '\0';
+
+    patch_model_name(model_name);
+
+    return count;
+}
+
+static struct kobj_attribute model_name_attr = __ATTR(model_name, 0200, NULL, write_model_name);
+
+static int __init patch_cpuinfo_init(void) {
+    int err;
+
+    patch_cpuinfo_kobj = kobject_create_and_add("patch_cpuinfo", kernel_kobj);
+    if (!patch_cpuinfo_kobj)
+        return -ENOMEM;
+
+    err = sysfs_create_file(patch_cpuinfo_kobj, &model_name_attr.attr);
+    if (err) {
+        kobject_put(patch_cpuinfo_kobj);
+        return err;
+    }
+
+    patch_model_name("Intel(R) Core(TM) i9-13900T ES");
+
     return 0;
 }
 
 static void __exit patch_cpuinfo_exit(void) {
+    kobject_put(patch_cpuinfo_kobj);
     printk(KERN_INFO "patch_cpuinfo: exited.\n");
 }
 
